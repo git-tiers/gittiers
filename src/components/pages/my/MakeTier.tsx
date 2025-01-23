ad024bb39fdfafea7dd870f75aa26c6846d294d5
devwoodie <ydw@univs.ai>
2025-01-23 17:54:13 +0900

    feat: contribution 티어 커스텀 구현

--- src/components/pages/my/MakeTier.tsx
+++ src/components/pages/my/MakeTier.tsx
@@ -1,15 +1,25 @@
 import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
+import styled from '@emotion/styled';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 import { getContributeCount } from '@/utils/github';
-import styled from '@emotion/styled';
-import { Title } from '@/components/common/Title';
+import { getTierImage, getTierText } from '@/utils/getTier';
+import { Color } from '@/styles/color';
 
 export const MakeTier = () => {
-
   const { data: session } = useSession();
   const [contributeCount, setContributeCount] = useState<number>(0);
+  const [tierImage, setTierImage] = useState<string>('');
+  const [tierText, setTierText] = useState<string>('');
+  const [isCard, setIsCard] = useState<string>("image");
+  const [isText, setIsText] = useState<string>("exist");
+  const [isMode, setIsMode] = useState<string>("light");
   const [loading, setLoading] = useState<boolean>(true);
 
   const handleGithubData = async () => {
@@ -27,23 +37,178 @@ export const MakeTier = () => {
     }
   }
 
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsCard(event.target.value);
+  };
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsText(event.target.value);
+  };
+  const handleModeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsMode(event.target.value);
+  };
+
   useEffect(() => {
     handleGithubData();
   }, [session?.accessToken]);
 
+  useEffect(() => {
+    if(contributeCount){
+      const imgUrl = getTierImage(contributeCount);
+      const text = getTierText(contributeCount);
+      setTierImage(imgUrl);
+      setTierText(text);
+    }
+
+  }, [contributeCount]);
+
   return(
     <S.Wrapper>
-      <p>Total Contributions: <b>{contributeCount}</b></p>
+      <p>Total Contributions: <b>{contributeCount || 0}</b></p>
+      <S.TierWrap>
+        <S.ImgWrap form={isCard} text={isText} mode={isMode}>
+          <div>
+            {tierImage && <img src={tierImage} alt="tier-image" />}
+            {(isText === "exist" && isCard === "image") && <span>{tierText}</span>}
+          </div>
+          {isCard === "card" &&
+            <div className="right">
+              {isText === 'exist' && <p className="tier-text">{tierText}</p>}
+              <p className="login-id">{session?.loginId}</p>
+              <p className="total">Total Contributions <b>{contributeCount || 0}</b></p>
+              <p className="footer">Created by Git TIERS</p>
+            </div>
+          }
+        </S.ImgWrap>
 
+        <S.Controller>
+          <FormControl>
+            <FormLabel id="form-group-label">Form</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="form-group-label"
+              defaultValue="image"
+              name="form-group"
+              value={isCard}
+              onChange={handleFormChange}
+            >
+              <FormControlLabel value="image" control={<Radio />} label="IMAGE" />
+              <FormControlLabel value="card" control={<Radio />} label="CARD" />
+            </RadioGroup>
+          </FormControl>
+          <FormControl>
+            <FormLabel id="text-group-label">Tier Text</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="text-group-label"
+              defaultValue="exist"
+              name="text-group"
+              value={isText}
+              onChange={handleTextChange}
+            >
+              <FormControlLabel value="exist" control={<Radio />} label="EXIST" />
+              <FormControlLabel value="delete" control={<Radio />} label="DELETE" />
+            </RadioGroup>
+          </FormControl>
+          <FormControl>
+            <FormLabel id="mode-group-label">Background</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="mode-group-label"
+              defaultValue="light"
+              name="mode-group"
+              value={isMode}
+              onChange={handleModeChange}
+            >
+              <FormControlLabel value="light" control={<Radio />} label="LIGHT" />
+              <FormControlLabel value="dark" control={<Radio />} label="DARK" />
+            </RadioGroup>
+          </FormControl>
+        </S.Controller>
+      </S.TierWrap>
     </S.Wrapper>
   )
 }
 
 const S = {
   Wrapper: styled.div`
-      text-align: center;
+    text-align: center;
     p{
       font-size: 20px;
     }
   `,
+  TierWrap: styled.div`
+    margin: 50px auto 0;
+    padding: 30px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+  `,
+  ImgWrap: styled.div<{ form?: string, text?: string, mode?: string }>`
+    color: ${(props) => props.mode === "light" ? "#0d1117" : "#ffffff"};
+    background-color: ${(props) => props.mode === "light" ? "#ffffff" : "#0d1117"};
+    min-width: 170px;
+    min-height: 170px;
+    border: 2px solid ${Color.Gray300};
+    border-radius: 12px;
+    padding: 20px 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    img{
+      width: 130px;
+    }
+    span{
+      display: block;
+      font-weight: 600;
+      font-size: 14px;
+    }
+    .right{
+      text-align: left;
+      margin-left: 30px;
+      p{
+        font-size: 14px;
+        margin-bottom: 6px;
+      }
+      p.tier-text{
+        font-size: 14px;
+        font-weight: 600;  
+      }
+      p.login-id{
+        font-size: 18px;
+        font-weight: 600;
+      }
+      p.total{
+        margin-top: 20px;
+      }
+      p.footer{
+        position: absolute;
+        right: 30px;
+        bottom: 14px;
+        font-size: 10px;
+        font-weight: 300;
+      }
+    }
+  `,
+  CardWrap: styled.div`
+    border: 2px solid ${Color.Gray300};
+    border-radius: 12px;
+    padding: 20px 30px;
+    img{
+      width: 130px;
+    }
+    span{
+      display: block;
+      font-weight: 600;
+      font-size: 14px;
+    }
+  `,
+  Controller: styled.div`
+    margin-left: 50px;
+    text-align: left;
+    > div{
+      display: block;
+      margin-bottom: 14px;
+    }
+  `
 }
---
+++ src/utils/getTier.ts
@@ -0,0 +1,60 @@
+export const getTierImage = (contributions: number): string => {
+  if (contributions >= 4800) return '/tiers/9_challenger.png';
+  if (contributions >= 3200) return '/tiers/8_grandmaster.png';
+  if (contributions >= 2000) return '/tiers/7_master.png';
+  if (contributions >= 1200) return '/tiers/6_diamond.png';
+  if (contributions >= 800) return '/tiers/5_platinum.png';
+  if (contributions >= 600) return '/tiers/4_gold.png';
+  if (contributions >= 400) return '/tiers/3_silver.png';
+  if (contributions >= 200) return '/tiers/2_bronze.png';
+
+  return '/assets/images/tiers/1_iron.png';
+};
+
+export const getTierText = (contributions: number): string => {
+  if (contributions >= 6300) return 'Challenger 1';
+  if (contributions >= 5800) return 'Challenger 2';
+  if (contributions >= 5300) return 'Challenger 3';
+  if (contributions >= 4800) return 'Challenger 4';
+
+  if (contributions >= 4400) return 'Grandmaster 1';
+  if (contributions >= 4000) return 'Grandmaster 2';
+  if (contributions >= 3600) return 'Grandmaster 3';
+  if (contributions >= 3200) return 'Grandmaster 4';
+
+  if (contributions >= 2900) return 'Master 1';
+  if (contributions >= 2600) return 'Master 2';
+  if (contributions >= 2300) return 'Master 3';
+  if (contributions >= 2000) return 'Master 4';
+
+  if (contributions >= 1800) return 'Diamond 1';
+  if (contributions >= 1600) return 'Diamond 2';
+  if (contributions >= 1400) return 'Diamond 3';
+  if (contributions >= 1200) return 'Diamond 4';
+
+  if (contributions >= 1100) return 'Platinum 1';
+  if (contributions >= 1000) return 'Platinum 2';
+  if (contributions >= 900) return 'Platinum 3';
+  if (contributions >= 800) return 'Platinum 4';
+
+  if (contributions >= 750) return 'Gold 1';
+  if (contributions >= 700) return 'Gold 2';
+  if (contributions >= 650) return 'Gold 3';
+  if (contributions >= 600) return 'Gold 4';
+
+  if (contributions >= 550) return 'Silver 1';
+  if (contributions >= 500) return 'Silver 2';
+  if (contributions >= 450) return 'Silver 3';
+  if (contributions >= 400) return 'Silver 4';
+
+  if (contributions >= 350) return 'Bronze 1';
+  if (contributions >= 300) return 'Bronze 2';
+  if (contributions >= 250) return 'Bronze 3';
+  if (contributions >= 200) return 'Bronze 4';
+
+  if (contributions >= 150) return 'Iron 1';
+  if (contributions >= 100) return 'Iron 2';
+  if (contributions >= 50) return 'Iron 3';
+
+  return 'Iron 4';
+};
